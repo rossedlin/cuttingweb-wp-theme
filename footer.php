7bc4fb8eaf18926bd55732c56a3817a09cb7e3ad
Ross Edlin <rossedlin@gmail.com>
2017-08-09 18:15:53 +0700

    improve the footer

--- footer.php
+++ footer.php
@@ -13,18 +13,22 @@
 
 	</div><!-- #content -->
 
+    <hr /> 
+
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cuttingweb' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cuttingweb' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cuttingweb' ), 'cuttingweb', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="row">
+                <div class="col-xs-4 text-left">
+					Copyright © <?= date('Y'); ?> Cutting Web
+                </div>
+				<div class="col-xs-4 text-center">
+
+                </div>
+				<div class="col-xs-4 text-right">
+					Created by <a href="http://www.rossedlin.co.uk/" target="_blank">Ross Edlin</a> | Powered by <a href="http://www.wordpress.org/" target="_blank">WordPress.org</a>
+                </div>
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
