9674ff4dac847f6015d6d914b40a85c4f1754c84
Ross Edlin <rossedlin@gmail.com>
2017-08-15 14:10:55 +0700

    add - sidebar recent

---
+++ sidebar-parts/recent-post.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 15/08/2017
+ * Time: 13:59
+ *
+ * @var WP_Post $post
+ */
+?>
+<li>
+	<a href="<?php //the_permalink() ?>"><?= $post->post_title ?></a>
+</li>
