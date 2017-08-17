eb3dd509e0c82e76407522151611be852a41388f
Ross Edlin <rossedlin@gmail.com>
2017-08-17 15:39:41 +0700

    adjust positioning of blog posts

--- template-home.php
+++ template-home.php
@@ -8,53 +8,48 @@
  * Template Name: Home
  */
 
+$args = array(
+	'numberposts'      => 10,
+	'offset'           => 0,
+	'category'         => 0,
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'post',
+	'post_status'      => 'publish',
+	'suppress_filters' => true,
+);
+
+/** @var WP_Post[] $posts */
+$posts = wp_get_recent_posts($args, OBJECT);
+
 get_header(); ?>
 
-	<div id="" class="" style="">
+	<div id="site-main" class="site-main container">
 
-		<main id="main" class="site-main">
+		<div class="row">
+			<div class="col-xs-12 col-md-9" style="padding: 0;">
+				<?php foreach ($posts as $post)
+				{
+					get_template_part('template-parts/home', 'post');
 
-			<?php
-			$args = array(
-				'numberposts'      => 10,
-				'offset'           => 0,
-				'category'         => 0,
-				'orderby'          => 'post_date',
-				'order'            => 'DESC',
-				'include'          => '',
-				'exclude'          => '',
-				'meta_key'         => '',
-				'meta_value'       => '',
-				'post_type'        => 'post',
-				'post_status'      => 'publish',
-				'suppress_filters' => true,
-			);
-
-			$posts = wp_get_recent_posts($args, OBJECT);
-			?>
-
-			<div class="row">
-				<div class="col-xs-12 col-md-9">
-					<?php foreach ($posts as $post)
+					// If comments are open or we have at least one comment, load up the comment template.
+					if (comments_open() || get_comments_number())
 					{
-						get_template_part('template-parts/home', 'post');
-
-						// If comments are open or we have at least one comment, load up the comment template.
-						if (comments_open() || get_comments_number())
-						{
-//					comments_template();
-						}
-
-//						break;
+//							comments_template();
 					}
-					?>
-				</div>
-				<div class="col-xs-12 col-md-3" style="margin: 20px 0">
-					<?php get_sidebar(); ?>
-				</div>
+				}
+				?>
 			</div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<div class="col-xs-12 col-md-3">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+
+	</div>
 
 <?php
 get_footer();
