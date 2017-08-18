672c37406179158bd6c00c2ec9153c0726be7dd5
Ross Edlin <rossedlin@gmail.com>
2017-08-18 13:59:11 +0700

    fix - footer

--- footer.php
+++ footer.php
@@ -11,26 +11,27 @@
 
 ?>
 
-		<hr />
-		
-		<footer id="colophon" class="site-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-12 text-center">
-						Copyright © <?= date('Y'); ?> Cutting Web
-					</div>
-					<div class="col-xs-12 text-center">
-						Created by <a href="http://www.rossedlin.co.uk/" target="_blank">Ross Edlin</a> | Powered by <a href="http://www.wordpress.org/" target="_blank">WordPress.org</a>
-					</div>
-				</div><!-- .site-info -->
-			</div>
-		</footer><!-- #colophon -->
+<hr/>
 
-	</div><!-- #content -->
+<footer id="colophon" class="site-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 text-center">
+				Copyright © <?= date('Y'); ?> Cutting Web
+			</div>
+			<div class="col-xs-12 text-center">
+				Created by <a href="http://www.rossedlin.co.uk/" target="_blank">Ross Edlin</a> | Powered by <a
+					href="http://www.wordpress.org/" target="_blank">WordPress.org</a>
+			</div>
+		</div><!-- .site-info -->
+	</div>
+</footer><!-- #colophon -->
+
+</div><!-- #content -->
 </div><!-- #page -->
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="/wp-content/themes/cuttingweb-underscore/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script src="/wp-content/themes/cuttingweb/bootstrap-3.3.7/js/bootstrap.min.js"></script>
 
 <?php wp_footer(); ?>
 
